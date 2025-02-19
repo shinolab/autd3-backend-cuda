c77d36d6a4624025d7be5d3753c3419865b9bc89
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-02-19 14:21:02 +0900

    bump to v29.0.0

--- examples/main.rs
+++ examples/main.rs
@@ -5,21 +5,22 @@ use autd3::prelude::*;
 use autd3_backend_cuda::CUDABackend;
 use autd3_gain_holo::*;
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let mut autd = Controller::builder([AUTD3::new(Vector3::zeros())])
-        .open(Nop::builder())
-        .await?;
+fn main() -> Result<()> {
+    let mut autd = Controller::open([AUTD3::default()], Nop::new())?;
 
     let backend = std::sync::Arc::new(CUDABackend::new()?);
 
     let center = autd.geometry().center() + Vector3::new(0., 0., 150.0 * mm);
     let p = Vector3::new(30. * mm, 0., 0.);
-    let g = GSPAT::new(backend, [(center + p, 5e3 * Pa), (center - p, 5e3 * Pa)]);
+    let g = GSPAT {
+        foci: vec![(center + p, 5e3 * Pa), (center - p, 5e3 * Pa)],
+        option: GSPATOption::default(),
+        backend,
+    };
 
-    autd.send(g).await?;
+    autd.send(g)?;
 
-    autd.close().await?;
+    autd.close()?;
 
     Ok(())
 }
