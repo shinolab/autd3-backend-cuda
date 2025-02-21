c54728c4a8597dac67bb102ff55da65765c5615c
shun suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2025-02-21 16:06:00 +0900

    update bindings

---
+++ src/cusolver/mod.rs
@@ -0,0 +1,15 @@
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(clippy::upper_case_acronyms)]
+#![allow(deref_nullptr)]
+
+#[cfg(target_os = "windows")]
+mod win32;
+#[cfg(target_os = "windows")]
+pub use win32::*;
+
+#[cfg(not(target_os = "windows"))]
+mod linux;
